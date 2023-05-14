eec6679c19a1e9ce0459df936286c8e3e08310ad
marc [itb] crusellas serra <marc.crusellas.7e4@itb.cat>
2023-05-14 18:17:41 +0200

    .

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,24 @@
 use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
 
+
+#[derive(Debug, serde::Deserialize)]
+struct User {
+    name: String,
+    password: String,
+}
+
+#[post("/new")]
+async fn create_user(user: web::Json<User>) -> impl Responder {
+    // In this example, we simply return the received user's name and password
+    HttpResponse::Ok().body(format!("Created user: {:?}", user))
+}
+
+#[get("/hello")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello, world!")
+}
+
+
 #[get("/hello")]
 async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hello, world!")
